323f996b4bb2ac409aca40a0a1aa86c0db5e8552
ios-teritum <ios@tertiumtechnology.it>
2018-06-29 13:31:32 +0200

    Added initial project structure

---
+++ RfidActiveApiSensorTestappIosObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RfidActiveApiSensorTestappIosObjC
+//
+//  Created by Stefano Crosara on 29/06/18.
+//  Copyright © 2018 Tertium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RfidActiveApiSensorTestappIosObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RfidActiveApiSensorTestappIosObjC
+//
+//  Created by Stefano Crosara on 29/06/18.
+//  Copyright © 2018 Tertium. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
